0a366aa6d80b32ef4393df2c9af37fcd13aa308a
Nick Barry <itsnickbarry@protonmail.ch>
2025-05-08 18:34:10 -0600

    remove unused config validation hook

--- src/hooks/config.ts
+++ src/hooks/config.ts
@@ -1,22 +1,11 @@
 import type { HardhatGitConfig } from '../types.js';
-import type {
-  ConfigHooks,
-  HardhatUserConfigValidationError,
-} from 'hardhat/types/hooks';
+import type { ConfigHooks } from 'hardhat/types/hooks';
 
 const DEFAULT_CONFIG: HardhatGitConfig = {
   npmInstall: 'npm install',
 };
 
 export default async (): Promise<Partial<ConfigHooks>> => ({
-  validateUserConfig: async (userConfig) => {
-    const errors: HardhatUserConfigValidationError[] = [];
-
-    // TODO: validate config
-
-    return errors;
-  },
-
   resolveUserConfig: async (userConfig, resolveConfigurationVariable, next) => {
     return {
       ...(await next(userConfig, resolveConfigurationVariable)),
