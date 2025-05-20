72f50a1aee39ff920c7f311f5d8aa5958d11131c
Nick Barry <itsnickbarry@protonmail.ch>
2025-05-19 23:02:39 -0600

    accept list of refs in git remove task

--- src/actions/git_remove.ts
+++ src/actions/git_remove.ts
@@ -1,26 +1,30 @@
-import pkg from '../../package.json';
 import { HardhatGitOrigin } from '../lib/hardhat_git.js';
 import type { NewTaskActionFunction } from 'hardhat/types/tasks';
 
 interface TaskActionArguments {
-  ref: string;
+  refs: string[];
 }
 
 const action: NewTaskActionFunction<TaskActionArguments> = async (
   args,
   hre,
 ) => {
-  const { ref } = args;
+  const { refs } = args;
 
   const origin = new HardhatGitOrigin(hre.config.paths.root);
-  const clone = await origin.checkout(ref);
 
-  if (await clone.isInitialized()) {
-    console.log(`Removing clone of ref ${clone.ref} at ${clone.directory}.`);
+  const clones = refs.length
+    ? await Promise.all(refs.map((ref) => origin.checkout(ref)))
+    : await origin.list();
+
+  for (const clone of clones) {
+    if (await clone.isInitialized()) {
+      console.log(`Removing clone of ref ${clone.ref} at ${clone.directory}.`);
+    }
+
+    // remove even if uninitialized in case of failed initialization
+    await clone.remove();
   }
-
-  // remove even if uninitialized in case of failed initialization
-  await clone.remove();
 };
 
 export default action;
--- src/tasks/git_remove.ts
+++ src/tasks/git_remove.ts
@@ -5,10 +5,11 @@ export default task(TASK_GIT_REMOVE)
   .setDescription(
     'Delete a clone of the current git repository corresponding to a given reference',
   )
-  .addPositionalArgument({
-    name: 'ref',
-    description: 'Git reference whose clone to delete',
-    defaultValue: 'HEAD',
+  .addVariadicArgument({
+    name: 'refs',
+    description:
+      'List of git references whose clones to delete (removes all clones by default)',
+    defaultValue: [],
   })
   .setAction(import.meta.resolve('../actions/git_remove.js'))
   .build();
