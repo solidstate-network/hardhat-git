e4691a812be7232d5c967a6bf7ef29756d07b01b
Nick Barry <itsnickbarry@protonmail.ch>
2025-05-19 22:36:09 -0600

    remove force option from git remove task

--- src/actions/git_remove.ts
+++ src/actions/git_remove.ts
@@ -1,32 +1,26 @@
 import pkg from '../../package.json';
 import { HardhatGitOrigin } from '../lib/hardhat_git.js';
-import { HardhatPluginError } from 'hardhat/plugins';
 import type { NewTaskActionFunction } from 'hardhat/types/tasks';
 
 interface TaskActionArguments {
   ref: string;
-  force: boolean;
 }
 
 const action: NewTaskActionFunction<TaskActionArguments> = async (
   args,
   hre,
 ) => {
-  const { ref, force } = args;
+  const { ref } = args;
 
   const origin = new HardhatGitOrigin(hre.config.paths.root);
   const clone = await origin.checkout(ref);
 
-  if (!force && !(await clone.isInitialized())) {
-    throw new HardhatPluginError(
-      pkg.name,
-      `Clone of ref ${ref} does not exist`,
-    );
+  if (await clone.isInitialized()) {
+    console.log(`Removing clone of ref ${clone.ref} at ${clone.directory}.`);
   }
 
+  // remove even if uninitialized in case of failed initialization
   await clone.remove();
-
-  console.log(`Deleted clone of ref ${clone.ref} at ${clone.directory}.`);
 };
 
 export default action;
